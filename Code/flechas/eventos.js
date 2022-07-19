8c7248a025b6dfe45db66421c7c8e8ccb6434c7f
Gabriel Cordero <107332315+GabrielCorderoDeveloper@users.noreply.github.com>
2022-07-19 12:00:50 -0400

    Create eventos.js

---
+++ Code/flechas/eventos.js
@@ -0,0 +1,14 @@
+var teclas = {
+   UP: 38,
+   DOWN: 40,
+   LEF: 37,
+   RIGHT; 39,
+}
+
+document.addEventListener("keyUP", dibujarTeclado);
+
+function dibujarTeclado(evento) {
+    if(evento.keycode == teclas.UP){
+        
+    }
+}
