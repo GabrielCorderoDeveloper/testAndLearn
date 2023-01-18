dc74de1186afffc62b995d4bfdbdce0a5e3c86dc
Gabriel Cordero <107332315+GabrielCorderoDeveloper@users.noreply.github.com>
2023-01-18 08:07:44 -0500

    Create array.js

---
+++ Code/W3 test and proyects/Java Script👨‍💻/array.js
@@ -0,0 +1,12 @@
+var frutas = ["Platano","Manzana","Cereza","Fresa"]
+
+//agregar elemento al final
+var masFrutas = frutas.push("Uvas")
+
+//eliminar elemento
+var ultimo = frutas.pop("Uvas");
+
+//agregar primer elemento
+var nuevaLongitud = frutas.unshift("Uvas")
+
+var posicion = frutas.
