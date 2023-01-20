cdd43eaea32d14c2248b5cfca495f39dfbc3279e
Gabriel Cordero <107332315+GabrielCorderoDeveloper@users.noreply.github.com>
2023-01-20 08:57:52 -0500

    Create switch.js

---
+++ Code/W3 test and proyects/Java Script👨‍💻/switch.js
@@ -0,0 +1,14 @@
+var numero = 1;
+
+switch (numero){
+    case 1:
+        console.log("Soy el primero");
+        break;
+    
+    case 10:
+    console.log("Soy el decimo");
+    break;   
+
+    default:
+        console.log("No soy nadita :,(")
+}
