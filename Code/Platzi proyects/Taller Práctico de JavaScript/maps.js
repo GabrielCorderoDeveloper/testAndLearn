17773444609390af2a1c167fbe6d799111cad2b3
Gabriel Cordero <107332315+GabrielCorderoDeveloper@users.noreply.github.com>
2023-01-23 08:22:53 -0500

    Create maps.js

---
+++ Code/Platzi proyects/Taller Práctico de JavaScript/maps.js
@@ -0,0 +1,66 @@
+/*
+ * Reglas:
+ * El final de cada nivel debe ser el inicio del siguiente
+*/
+
+const emojis = {
+    '-': ' ',
+    'O': '🚪',
+    'X': '🐍',
+    'I': '💸',
+    'PLAYER': '👨🏻‍🦯',
+    'BOMB_COLLISION': '🔥',
+    'GAME_OVER': '👎🏻',
+    'WIN': '🏆',
+  };
+  
+  const maps = [];
+  maps.push(`
+    IXXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    OXXXXXXXXX
+  `);
+  maps.push(`
+    O--XXXXXXX
+    X--XXXXXXX
+    XX----XXXX
+    X--XX-XXXX
+    X-XXX--XXX
+    X-XXXX-XXX
+    XX--XX--XX
+    XX--XXX-XX
+    XXXX---IXX
+    XXXXXXXXXX
+    `);
+  maps.push(`
+    I-----XXXX
+    XXXXX-XXXX
+    XX----XXXX
+    XX-XXXXXXX
+    XX-----XXX
+    XXXXXX-XXX
+    XX-----XXX
+    XX-XXXXXXX
+    XX-----OXX
+    XXXXXXXXXX
+  `);
+
+  maps.push(`
+  IX--------
+  ---XXXXXX-
+  -XXXXX----
+  -XX----XXX
+  -XX-XXXX-X
+  -XX-X----X
+  --X---XX-X
+  -X--XXXX--
+  X--XXXXO--
+  --X------X
+`);
