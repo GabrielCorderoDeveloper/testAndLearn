2ffe70d67bc17a82be2304df44221563c4f17df7
Gabriel Cordero <107332315+GabrielCorderoDeveloper@users.noreply.github.com>
2023-01-26 08:04:14 -0500

    Create Objetos literales y prototipos.js

---
+++ Code/Platzi proyects/Curso POO/Objetos literales y prototipos.js
@@ -0,0 +1,14 @@
+const natalia = {
+    name: "Natalia",
+    edad: 20,
+    cursosAprobados: [
+        "Curso definitivo html y css",
+        "Curso practico html y css"
+    ],
+}
+
+//Hacer que natalia apruebe otro curso
+
+natalia.cursosAprobados.push("Curso de responsive design");
+
+console.log(natalia);
