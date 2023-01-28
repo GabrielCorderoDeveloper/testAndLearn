ed1c1201e4c5ed2171d54daa65b56059d52d5afd
Gabriel Cordero <107332315+GabrielCorderoDeveloper@users.noreply.github.com>
2023-01-28 07:52:52 -0500

    Create else if.js

---
+++ Code/W3 test and proyects/Java Script👨‍💻/else if.js
@@ -0,0 +1,15 @@
+let age = 18;
+
+if (age == 21){
+    console.log("Yey, you can vote!");
+} else if(age > 21){
+    console.log("You can vote again!");
+} else {
+    console.log("You cannot vote yet :(");
+}
+
+//else if in one sentence
+
+let number = 1;
+let result = number == 1 ? "Yes i'm the one!" : "I'm not the one :,(";
+console.log(result);
