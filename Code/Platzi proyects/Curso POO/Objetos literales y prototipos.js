4b99276e37c8e7e5cf91bcb4f6c3003d5396fa17
Gabriel Cordero <107332315+GabrielCorderoDeveloper@users.noreply.github.com>
2023-01-28 07:52:54 -0500

    Update Objetos literales y prototipos.js

--- Code/Platzi proyects/Curso POO/Objetos literales y prototipos.js
+++ Code/Platzi proyects/Curso POO/Objetos literales y prototipos.js
@@ -1,59 +1,27 @@
-// const natalia = {
-//     name: "Natalia",
-//     edad: 20,
-//     cursosAprobados: [
-//         "Curso definitivo html y css",
-//         "Curso practico html y css"
-//     ],
-//     aprobarCurso(NuevoCurso) {
-//         this.cursosAprobados.push(NuevoCurso);
-//     }
-// }
-
-//Hacer que natalia apruebe otro curso
-// console.log(natalia);
-// natalia.aprobarCurso("Flex box")
-// console.log(natalia);
-
-function Student(name, age, cursosAprobados) {
-    this.name = name;
-    this.age = age;
-    this.cursosAprobados = cursosAprobados;
-}
-
-Student.prototype.aprobarCurso = function(nuevoCurso) {
-    this.cursosAprobados.push(nuevoCurso);
-}
-
-const juanita = new Student(
-    "Juanita Alejandra",
-    18,
-    [
-        "Css grid",
-        "Html basico"
-    ]
-);
-
-console.log(juanita);
-
 //Prototipos con sintaxis de clases
+
 class Student2 {
-    constructor(name, age, cursosAprobados) {
+    constructor({
+        name,
+        age,
+        email,
+        cursosAprobados = [],
+    }) {
         this.name = name;
         this.age = age;
         this.cursosAprobados = cursosAprobados;
+        this.email = email;
     }
-
-    aprobarCurso(nuevoCurso){
-        this.cursosAprobados.push(nuevoCurso);
-    }
+         aprobarCurso(NuevoCurso) {
+         this.cursosAprobados.push(NuevoCurso);
+     }
 }
 
-const miguelito = new Student2(
-    'Miguel',
-    28,
-    [
-        "Analisis de negocios",
-        "Principios de visualizacion de datos",
-    ]
-)
+
+const miguelito = new Student2({
+        name: "Miguel",
+        age: 23,
+        email: "miguelito@platzi.com",
+    });
+
+   
