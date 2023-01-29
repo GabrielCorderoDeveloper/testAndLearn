23dd45a8f60065b67d3ad9f2b1ba8902f2429584
Gabriel Cordero <107332315+GabrielCorderoDeveloper@users.noreply.github.com>
2023-01-29 06:48:11 -0500

    Create find.js

---
+++ Code/Platzi proyects/Curso de Manipulación de Arrays en JavaScript/find.js
@@ -0,0 +1,25 @@
+const products = [
+    {
+      name: "Pizza",
+      price: 12,
+      id: '🍕'
+    },
+    {
+      name: "Burger",
+      price: 23,
+      id: '🍔'
+    },
+    {
+      name: "Hot dog",
+      price: 34,
+      id: '🌭'
+    },
+    {
+      name: "Hot cakes",
+      price: 355,
+      id: '🥞'
+    },
+  ];
+
+  rta = products.find(item => item.id === '🥞');
+  console.log(rta);
