69e927e65220289bdd1a24cec02fad9f05885516
Gabriel Cordero <107332315+GabrielCorderoDeveloper@users.noreply.github.com>
2023-02-01 13:31:32 -0500

    Create map.js

---
+++ Code/Platzi proyects/Curso de Manipulación de Arrays en JavaScript/map.js
@@ -0,0 +1,20 @@
+/*
+const letters = ["a", "b", "c", "d"];
+const newArray = letters.map(item => item + "+");
+
+for (let index = 0; index < letters.length; index++) {
+    const element = letters[index];
+    newArray.push(element + "+");
+} 
+
+console.log("Original", letters);
+console.log("New", newArray);
+*/
+
+const numbers = [2, 3, 7, 8, 4, 2, 7];
+
+function multiplyBy2(one, two, three, four, five, six, seven) {
+    return one , two , three, four, five,  six, seven * 2
+}
+
+console.log(multiplyBy2(2));
