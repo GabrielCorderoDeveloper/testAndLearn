596eb0733d0f21867b50294b16981eea4c524b57
GabrielCorderoDeveloper <gabrielcorderoolivo@gmail.com>
2023-02-02 10:03:01 -0500

    Create dibujo.js

---
+++ Code/Proyectos viejos/no utiles/canvas/dibujo.js
@@ -0,0 +1,35 @@
+var texto = document.getElementById("texto_lineas");
+var boton= document.getElementById("botoncito");
+var d = document.getElementById("dibujito");
+boton.addEventListener("click", dibujoPorClick)
+var ancho = d.width;
+var lienzo = d.getContext("2d");
+
+function dibujoPorClick (){
+var lineas = parseInt(texto.value);
+var l = 0;
+var yi, xf;
+var colorcito = "#FAA";
+var espacio = ancho/lineas;
+
+while(l < lineas) {
+  yi = espacio * l;
+  xf = espacio * (l + 1);
+  dibujarLinea(colorcito, 0 , yi, xf, 300);
+  console.log("Linea " + 1);
+  l ++
+}
+
+dibujarLinea(colorcito, 1,1,1,299);
+dibujarLinea(colorcito, 1,299,299,299);
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal){
+lienzo.beginPath();
+lienzo.strokeStyle = color;
+lienzo.moveTo(xinicial, yinicial);
+lienzo.lineTo(xfinal, yfinal);
+lienzo.stroke();
+lienzo.closePath();
+}
+
+}
