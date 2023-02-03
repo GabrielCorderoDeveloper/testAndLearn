a1f2b8bf91df2bc871e93a464abd867d5bdf7f1c
GabrielCorderoDeveloper <gabrielcorderoolivo@gmail.com>
2023-02-03 09:45:20 -0500

    Create Métodos estáticos del prototipo Object.js

---
+++ Code/Platzi proyects/Curso Intermedio de Programación Orientada a Objetos en JavaScript/Métodos estáticos del prototipo Object.js
@@ -0,0 +1,44 @@
+const juan = {
+    name: "Juanito",
+    age: 18,
+    approvedCourses: ["Curso 1"],
+
+    addCourse(newCourse){
+        this.approvedCourses.push(newCourse);
+    }
+};
+
+// console.log(Object.keys(juan));
+// console.log(Object.getOwnPropertyNames(juan));
+// console.log(Object.entries(juan));
+
+Object.defineProperty(juan, "navigator" , {
+    value: "Chrome",
+    enumerable: false,
+    writable: true,
+    configurable: true,
+});
+
+
+Object.defineProperty(juan, "editor" , {
+    value: "VSCode",
+    enumerable: true,
+    writable: false,
+    configurable: true,
+});
+
+Object.defineProperty(juan, "terminal" , {
+    value: "WSL",
+    enumerable: true,
+    writable: true,
+    configurable: false,
+});
+
+Object.defineProperty(juan, "pruebaNasa" , {
+    value: "extraterrestres",
+    enumerable: false,
+    writable: false,
+    configurable: false,
+});
+
+console.log(Object.getOwnPropertyDescriptors(juan));
