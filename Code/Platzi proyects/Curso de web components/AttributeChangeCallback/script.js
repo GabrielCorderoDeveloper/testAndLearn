e0e3540fbf2e4f54e7e1261455ae10b673be2c4e
GabrielCorderoDeveloper <gabrielcorderoolivo@gmail.com>
2023-02-14 11:02:51 -0500

    Create script.js

---
+++ Code/Platzi proyects/Curso de web components/AttributeChangeCallback/script.js
@@ -0,0 +1,56 @@
+class myElement extends HTMLElement{
+  
+    constructor(){
+      super();
+      //Venimos al constructor que es donde generamos la instancia de esta api
+        //Agregamos shadow down (API) y lo ponemos en modo open
+        this.attachShadow({mode:"open"})
+    }
+    static get observedAttributes() {
+        return ["title","parrafo","img"];
+    }
+    attributeChangedCallback(atribute, aldValue, newValue) {
+        if(atribute === "title") {
+            this.title = newValue;
+        }
+        if(atribute === "parrafo") {
+            this.parrafo = newValue;
+        }
+        if(atribute === "img") {
+            this.img = newValue;
+        }
+    }
+    getTemplate(){
+      const template = document.createElement('template');
+      template.innerHTML = `
+        <section>
+          <h2>${this.title}</h2>
+          <div>
+            <p>${this.parrafo}</p>
+            <img src="${this.img}"/>
+          </div>
+        </section>
+        ${this.getStyles()}
+      `;
+      return template
+    }
+    getStyles(){
+      return`
+        <style>
+          h2{
+            color:red;
+          }
+        </style>
+      `
+    }
+    render(){
+      //Ahora poder renderizar nuestros templates tenemos que cambiar el contexto
+       //Donde agregamos nuestro template ya que lo estabamos agregando al dom global
+        //Ahora debemos agregarlo en nuestro shadow dom que es otro contexto diferene
+      this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true))
+    }
+    connectedCallback(){
+      this.render();
+    }
+  }
+  customElements.define('my-element', myElement);
