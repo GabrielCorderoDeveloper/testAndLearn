d071b4d6dd484988ddf6f67525a5632f0e8bebca
GabrielCorderoDeveloper <gabrielcorderoolivo@gmail.com>
2023-02-22 13:03:54 -0500

    Create my-element.js

---
+++ Code/Platzi proyects/Curso de web components/slotted/my-element.js
@@ -0,0 +1,41 @@
+class myElement extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" })
+    }
+    getTemplate() {
+        const template = document.createElement("template");
+        template.innerHTML =`
+        <section>
+            <h1>
+                <slot name="title"></slot>
+            </h1>
+            <p>
+                <slot name="parrafo"></slot>
+            </p>
+            <slot></slot>
+        </section>
+        
+        ${this.getStyles()}
+        `;
+
+        return template;
+    }
+    getStyles() {
+        return `
+        <style>
+           ::slotted(span) {
+            font-size: 50px;
+            color: red;
+           }
+        </style>`;
+
+    }
+    render() {
+        this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
+    }
+    connectedCallback() {
+        this.render();
+    }
+}
+customElements.define("my-element", myElement);
