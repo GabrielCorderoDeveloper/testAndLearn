8c534385b83340116f08e14a01a221883c738467
GabrielCorderoDeveloper <gabrielcorderoolivo@gmail.com>
2023-03-03 08:32:51 -0500

    Create TaskCounter.js

---
+++ Code/all-react-code/my-todo/src/components/TaskCounter.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ProgressBar from 'react-bootstrap/ProgressBar';
+
+const TaskCounter = ({ todos }) => {
+  let totalTasks = todos.length;
+  let completedTasks = 1;
+
+  let now = (Math.floor((completedTasks / totalTasks) * 100));
+
+  return (
+    <ProgressBar 
+    variant='success' 
+    style={{ height: '30px'}} 
+    now={now} 
+    label={`${completedTasks} of ${totalTasks} tasks completed`}
+    valuemin={0} valuemax={100}  
+    className='my-2 col-md-7'/>
+  )
+}
+
+export default TaskCounter
