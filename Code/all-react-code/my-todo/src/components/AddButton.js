2acc74a8d682917c3540f888ce0681a599ae0b5c
GabrielCorderoDeveloper <gabrielcorderoolivo@gmail.com>
2023-03-03 08:32:44 -0500

    Create AddButton.js

---
+++ Code/all-react-code/my-todo/src/components/AddButton.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { MdAddCircle } from "react-icons/md";
+
+
+const SearchButton = () => {
+  return (
+    <span className='col-1'>
+      <MdAddCircle className='main-button' size="50px"/>
+    </span>
+  )
+}
+
+export default SearchButton
+
