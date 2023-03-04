447a78019136d75ae8e2bae10be01a143a32216b
GabrielCorderoDeveloper <gabrielcorderoolivo@gmail.com>
2023-03-04 07:30:25 -0500

    Update RemoveChecked.js

--- Code/all-react-code/my-todo/src/components/RemoveChecked.js
+++ Code/all-react-code/my-todo/src/components/RemoveChecked.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import { Button } from 'react-bootstrap'
 import { GrFormClose } from "react-icons/gr";
 
-const RemoveChecked = () => {
+const RemoveChecked = ({ clearTodos }) => {
   return (
-    <button className='btn btn-outline-success mt-md-0 mt-2'>
-        Remove cheched<GrFormClose size="25px" className='icon-fix'/>
-        </button>
+    <button className='btn btn-outline-success mt-md-0 mt-2' onClick={clearTodos}>
+      Remove cheched<GrFormClose size="25px" className='icon-fix'/>
+    </button>
   )
 }
 
