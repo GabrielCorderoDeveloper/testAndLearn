2e86e99114bf4faceb3d6afc0f785586ffffcd11
GabrielCorderoDeveloper <gabrielcorderoolivo@gmail.com>
2023-03-04 07:30:27 -0500

    Update AddButton.js

--- Code/all-react-code/my-todo/src/components/AddButton.js
+++ Code/all-react-code/my-todo/src/components/AddButton.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { MdAddCircle } from "react-icons/md";
-
+import  handleAddTodo from './SearchBar'
 
 const SearchButton = () => {
+
   return (
     <span className='col-1'>
-      <MdAddCircle className='main-button' size="50px"/>
+      <MdAddCircle onClick={handleAddTodo} className='main-button' size="50px"/>
     </span>
   )
 }
