8dfc8d2064e706bf9ee64cdae324427360ab195f
GabrielCorderoDeveloper <gabrielcorderoolivo@gmail.com>
2023-03-05 08:27:03 -0500

    Create JavaScript.js

---
+++ Code/JavaScript.js
@@ -0,0 +1,38 @@
+
+
+
+let authenticated = false;
+function happyMessage() { console.log('You are authenticated'); }
+function sadMessage() { console.log('You are NOT authenticated'); }
+
+// if (authenticated) {
+//     happyMessage()
+//   } else {
+//     sadMessage()
+//   }
+//~ Let's do it in a different way
+
+authenticated ? happyMessage() : sadMessage()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
