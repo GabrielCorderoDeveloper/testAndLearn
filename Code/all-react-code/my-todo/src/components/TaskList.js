cff9fc35d89338832a87ef5a746acfbff9a1a15f
GabrielCorderoDeveloper <gabrielcorderoolivo@gmail.com>
2023-03-05 08:26:56 -0500

    Update TaskList.js

--- Code/all-react-code/my-todo/src/components/TaskList.js
+++ Code/all-react-code/my-todo/src/components/TaskList.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Todo from './Todo'
 
-const TaskList = ({ todos }) => {
+const TaskList = ({ todos, toggleTodo }) => {
   return (
 
       todos.map(todo => {
-        return <Todo key={todo.id} todo={todo}/>
+        return <Todo key={todo.id} toggleTodo={toggleTodo} todo={todo}/>
       })
 
   )
