0550bf9a4b9732a2f4ffcd3553c9f01c6211556e
GabrielCorderoDeveloper <gabrielcorderoolivo@gmail.com>
2023-03-05 08:26:58 -0500

    Update Todo.js

--- Code/all-react-code/my-todo/src/components/Todo.js
+++ Code/all-react-code/my-todo/src/components/Todo.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
-const Todo = ({ todo }) => {
+const Todo = ({ todo, toggleTodo }) => {
+  function handleTodoClick() {
+    toggleTodo(todo.id)
+  }
+
   return (
     <div className='todo-item'>
         <label>
-            <input type='checkbox' checked={todo.complete}/>
-            {todo.name}
+            <input className='check' type='checkbox' checked={todo.complete} onChange={handleTodoClick}/>
+         <span className='todo-text'>{todo.name}</span>
 
         </label>
     </div>
