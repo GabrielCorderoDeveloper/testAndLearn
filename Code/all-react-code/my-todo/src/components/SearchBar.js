4217cf1061667c0d64749398e682b0d4ede6958b
GabrielCorderoDeveloper <gabrielcorderoolivo@gmail.com>
2023-03-05 08:26:54 -0500

    Update SearchBar.js

--- Code/all-react-code/my-todo/src/components/SearchBar.js
+++ Code/all-react-code/my-todo/src/components/SearchBar.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useRef } from 'react';
 
 const SearchBar = () => {
+  const todoNameRef = useRef();
+
+  function handleAddTodo() {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    console.log(name)
+  }
+
+
   return (
     <div className='col-9'>
-      <input className='form-control' type='text' placeholder='Add a task...'/>
+      <input onClick={handleAddTodo} ref={todoNameRef} className='form-control' type='text' placeholder='Add a task...'/>
     </div>
   )
 }
