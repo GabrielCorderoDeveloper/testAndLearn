54ccde74fec263d061a641d1a796bec61a043b07
GabrielCorderoDeveloper <gabrielcorderoolivo@gmail.com>
2023-03-07 07:57:30 -0500

    Update Counter.jsx

--- Code/all-react-code/m-crash/src/components/Counter.jsx
+++ Code/all-react-code/m-crash/src/components/Counter.jsx
@@ -1,44 +1,47 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  constructor() {
-    super();
-    this.state = {
-      count: 0,
-      tags: ['tag1', 'tag2', 'tag3']
-    };
-  }
-
+  // state = {
+  //   value: this.props.value,
+  // };
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.props.counter;
     return count === 0 ? "Zero" : count;
   }
-
-  handleIncrement() {
-    this.setState({ count: this.state.count + 1 });
+  // tagDetector() {
+  //   // if (this.state.tags.length === 0) return <p>There are no tags :/</p>
+  //   return this.state.tags.map(tag => <li key={tag} >{tag}</li>)
+  // }
+  getBadgeClass() {
+    let classes = 'badge p-1 m-2 bg-';
+    classes += (this.props.counter.value === 0) ? 'warning' : 'primary';
+    return classes
+  }
+  // handleIncrement() {
+  //   console.log();
+  //   this.setState({ value: this.state.value + 1 });
+  // };
+  doHandleIncrement = () => {
+    this.handleIncrement({ id: 1});
   };
 
-  tagDetector() {
-    if (this.state.tags.length === 0) return <p>There are no tags :/</p>
-
-    return this.state.tags.map(tag => <li key={tag} >{tag}</li>)
-  }
-
-  getBadgeClass() {
-    this.classes = 'badge p-1 m-2 bg-';
-    this.classes += (this.state.count === 0) ? 'warning' : 'primary';
-    return this.classes
-  }
+  handleIncrement = () => {
+    this.props.onIncrement(this.props.counter);
+  };
 
   render() {
     return (
-      <>
+      <div>
         <span className={this.getBadgeClass()}>{this.formatCount()}</span>
-        <button onClick={this.handleIncrement.bind(this)} className='btn btn-secondary btn-sm'>Increment</button>
-        <ul>
-          {this.tagDetector()}
-        </ul>
-      </>
+
+        <button onClick={this.handleIncrement} className='btn btn-secondary btn-sm'>
+          Increment
+        </button>
+
+        <button onClick={() => this.props.onDelete(this.props.counter.id)} className='btn btn-danger btn-sm m-2'>
+          Delete
+        </button>
+      </div>
     );
   }
 }
