1bdeb38f6eabe8525e1fc348cb8a97e220cb97fa
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2023-03-31 19:53:12 -0400

    Update App.tsx

--- Code/react-type/mosh-react-type-course/src/App.tsx
+++ Code/react-type/mosh-react-type-course/src/App.tsx
@@ -1,12 +1,31 @@
-import Expandabletext from "./components/Expandabletext"
-import Form from './components/Form'
+import { useState } from 'react'
+import ExpenseList from './expense-tracker/components/ExpenseList'
+import ExpenseFilter from './expense-tracker/components/ExpenseFilter';
+import ExpenseForm from './expense-tracker/components/ExpenseForm';
+
+export const categories = ['Groceries', 'Utilities', 'Entertainment'];
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState('')
+  const [expenses, setExpenses] = useState([
+    {id: 1, description: 'aaa', amount: 10, category: 'Utilities'},
+    {id: 2, description: 'aajha', amount: 32, category: 'Utilities'},
+    {id: 3, description: 'wgu', amount: 94, category: 'Utilities'},
+    {id: 4, description: 'a78aa', amount: 12, category: 'Utilities'},
+  ]);
+
+  const visibleExpesnses = selectedCategory ? expenses.filter(e => e.category === selectedCategory) : expenses;
 
   return (
-    <>
-      <Form />
-    </>
+    <div>
+      <div className='mb-5'>
+        <ExpenseForm/>
+      </div>
+      <div className='mb-3'>
+      <ExpenseFilter onSelectCategory={category => setSelectedCategory(category)}/>
+      </div>
+      <ExpenseList expenses={visibleExpesnses} onDelete={(id) => setExpenses(expenses.filter(e => e.id !== id))}/>
+    </div>
   )
 }
 
