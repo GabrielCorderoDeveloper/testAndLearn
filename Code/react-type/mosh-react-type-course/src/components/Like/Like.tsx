1a1522e3ee390c4cf6745ffb3f42ee94b5a50546
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2023-04-01 11:24:28 -0400

    Create Like.tsx

---
+++ Code/react-type/mosh-react-type-course/src/components/Like/Like.tsx
@@ -0,0 +1,20 @@
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai'
+import { useState } from 'react';
+
+interface Props {
+    onClick: () => void;
+}
+
+const Like = ({onClick}: Props) => {
+    const [status, setStatus] = useState(false);
+
+    const toggle = () => {
+        setStatus(!status);
+        onClick();
+    }
+
+    if (status) return <AiFillHeart color='red' size='40' onClick={toggle}/>
+    return <AiOutlineHeart color='red' size='40' onClick={toggle}/>
+}
+
+export default Like
