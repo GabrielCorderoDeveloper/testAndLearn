d38d30c9d328a67964382c2f18ee8fdc6f94edea
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2023-04-02 10:11:03 -0400

    Create ProductList.tsx

---
+++ Code/react-type/mosh-react-type-course/src/expense-tracker/components/ProductList.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react'
+
+const ProductList = ({ category }: {category: string}) => {
+    const [products, setProducts] = useState<string[]>([]);
+
+    useEffect(() => {
+        console.log('Fetching products in', category);
+        setProducts(['Clothing', 'HouseHold']);
+    }, [category]);
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProductList
