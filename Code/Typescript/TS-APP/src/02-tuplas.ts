50eb3b214301c0657bc03f08830a2dcc5026e71a
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2023-04-06 21:39:26 -0400

    Create 02-tuplas.ts

---
+++ Code/Typescript/TS-APP/src/02-tuplas.ts
@@ -0,0 +1,10 @@
+const prices: (number | string)[] = [1,2,4,5, 'zz'];
+prices.push(1);
+prices.push('1');
+
+
+//? const user: [string, number] = ['nicobytes', 12];
+let user: [string, number];
+user = ['nicobytes', 12];
+user = ['ddsggd', 33];
+
