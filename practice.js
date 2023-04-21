a150beebc5784d40224a54c048b1f103a3534426
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2023-04-21 12:22:51 -0400

    Create practice.js

---
+++ practice.js
@@ -0,0 +1,37 @@
+var containsDuplicate = function(nums) {   
+    let hTable = {};
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] in hTable) {
+            return true;
+        } else {
+            hTable[nums[i]] = 1;
+        }
+    }
+    return false
+};
+
+let test1 = [1, 2, 3, 1];
+let test2 = [1, 2, 3, 4];
+
+console.log(containsDuplicate(test2))
+
+
+
+//2! FizzBuzz ------------------------------>>>>>
+
+//2? For loop solution ---------------->
+for (let i =1; i < 101; i++) {
+    if (i % 15 === 0) {
+        console.log('FIZZBUZZ');
+    } else if (i % 3 === 0) {
+        console.log('FIZZ');
+    } else if (i % 5 === 0) {
+        console.log('BUZZ');
+    } else {
+        console.log(i);
+    }
+}
+
+//2? While loop solution ---------------->
+
+//2? Do while loop solution ---------------->
