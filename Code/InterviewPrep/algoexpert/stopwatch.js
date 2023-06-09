d841b4c206f0e401531862ee6472ee7aabac1f4d
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2023-06-09 09:54:56 -0400

    Stopwatch preactice.

---
+++ Code/InterviewPrep/algoexpert/stopwatch.js
@@ -0,0 +1,41 @@
+let timerID;
+let lastTimerStartTime = 0;
+
+const timer = document.getElementById('timer');
+const start = document.getElementById('start-button');
+const stop = document.getElementById('stop-button');
+const reset = document.getElementById('reset-button');
+
+start.addEventListener("click", startTime);
+stop.addEventListener('click', stopTime);
+reset.addEventListener('click', resetTime);
+
+function startTime() {
+  start.disabled = true;
+  stop.disabled = false;
+  reset.disabled = true;
+
+  lastTimerStartTime = Date.now();
+  timerID = requestAnimationFrame(updateTimer);
+}
+
+function stopTime() {
+  start.disabled = false;
+  reset.disabled = false;
+  stop.disabled = true;
+  cancelAnimtionFrame(timerID);
+}
+
+function resetTime() {
+  reset.disabled = true;
+  timer.textContent = '00:00:000'
+}
+
+function updateTimer() {
+  const millisElapsed = Date.now() - lastTimerStartTime;
+  const secondsElapsed = millisElapsed / 1000;
+  const minutesElapsed = secondsElapsed / 60;
+
+  
+}
+
