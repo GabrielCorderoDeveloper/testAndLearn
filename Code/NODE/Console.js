cc89134b099cd1371945f51564488b8f6fd4ad87
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2023-07-12 10:55:17 -0400

    Create Console.js

---
+++ Code/NODE/Console.js
@@ -0,0 +1,17 @@
+console.error('I am an error');
+
+const table = [
+    {
+        a: 1,
+        b: 'First'
+    },
+    {
+        a: 2,
+        b: 'Second'
+    }
+];
+
+console.table(table);
+
+console.count('Hi') 
+console.count('Hi')
