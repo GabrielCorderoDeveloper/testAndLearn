eec0f82a1af74810fdfe717154299465dd5122a2
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2023-07-23 11:24:57 -0400

    Update test.sql

--- Code/NODE/nodeMDB/test.sql
+++ Code/NODE/nodeMDB/test.sql
@@ -1,12 +1,46 @@
 CREATE TABLE books(
-   book_id INTEGER UNSIGNED PRIMARY AUTO_INCREMENT,
-   author ,
-   title VARCHAR(100) NOT NULL,
-   year INTEGER UNSIGNED NOT NULL DEFAULT 1990,
-   language VARCHAR(2) NOT NULL DEFAULT 'es' COMMENT 'ISO 639',
-   cover_url VARCHAR(500),
-   price DOUBLE(6,2) NOT NULL DEFAULT 10.0,
-   sellabe TINYINT(1) DEFAULT 1,
-   copies INTEGER NOT NULL 1,
-   description TEXT
+    book_id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    author_id INTEGER UNSIGNED ,
+    title VARCHAR(100) NOT NULL,
+    year INTEGER UNSIGNED NOT NULL DEFAULT 1990,
+    language VARCHAR(2) NOT NULL DEFAULT 'es' COMMENT 'ISO 639',
+    cover_url VARCHAR(500),
+    price DOUBLE(6,2) NOT NULL DEFAULT 10.0,
+    sellabe TINYINT(1) DEFAULT 1,
+    copies INTEGER NOT NULL DEFAULT 1,
+    description TEXT
 );
+
+CREATE TABLE IF NOT EXISTS authors (
+    author_id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NUll,
+    nationality VARCHAR (3)
+);
+
+CREATE TABLE clients (
+    client_id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    birthdate DATETIME,
+    gender ENUM('M', 'F', 'ND') NOT NULL,
+    active TINYINT(1) NOT NULL DEFAULT 1,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- Practica o ejercicio de platzi
+CREATE TABLE IF NOT EXISTS operations (
+    operation_id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    book_id INTEGER UNSIGNED,
+    client_id INTEGER UNSIGNED,
+    type ENUM('Prestado', 'Devuelto', 'Vendido') NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    finished TINYINT(1) NOT NULL
+);
+-- ----------------------------->
+
+
+-- Insertando valores
+INSERT INTO authors(author_id, name, nationality) 
+VALUES('4567', 'Juan Costas', 'MEX');
