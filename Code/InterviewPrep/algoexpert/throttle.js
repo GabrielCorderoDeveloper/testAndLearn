54363620421327d5e3ff475738a9061d41d16e13
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2023-08-14 11:31:39 -0400

    Update throttle.js

--- Code/InterviewPrep/algoexpert/throttle.js
+++ Code/InterviewPrep/algoexpert/throttle.js
@@ -1,5 +1,14 @@
 function throttle(callback, delay) {
+    let lastCalledTime = 0;
+
     const throttledFunction = function(...args) {
-        
+        const currentTime = Date.now();
+        const timeSinceLastCall = currentTime - lastCalledTime;
+        const daelayRemaining = delay - timeSinceLastCall;
+
+        if (daelayRemaining <= 0) {
+            lastCalledTime = currentTime;
+            callback.apply(this, args);
+        }
     }
   }
