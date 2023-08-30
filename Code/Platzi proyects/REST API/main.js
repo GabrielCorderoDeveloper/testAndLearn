c174053b454036caff65a226b062dff25da62df1
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2023-08-30 16:15:48 -0400

    Update main.js

--- Code/Platzi proyects/REST API/main.js
+++ Code/Platzi proyects/REST API/main.js
@@ -1,19 +1,27 @@
-console.log('Hello, world')
+const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=live_6Flh15QYDYu9XYjnQk3WCT514NX5D0XOqXAttwuzCtYCAwg87fIEvLu3IgPdzsXL';
+const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=live_6Flh15QYDYu9XYjnQk3WCT514NX5D0XOqXAttwuzCtYCAwg87fIEvLu3IgPdzsXL';
 
-const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3';
-
-async function reload() {
-  const res = await fetch(API_URL);
+async function loadRandomMichis() {
+  const res = await fetch(API_URL_RANDOM);
   const data = await res.json();
 
   console.log(data)
   const img1 = document.getElementById('img1');
   const img2 = document.getElementById('img2');
-  const img3 = document.getElementById('img3');
   
   img1.src = data[0].url;
   img2.src = data[1].url;
-  img3.src = data[2].url;
 }
 
-reload();
+
+
+async function loadFavoritesMichis() {
+  const res = await fetch(API_URL_FAVORITES);
+  const data = await res.json();
+
+  console.log(data)
+
+}
+
+loadRandomMichis();
+loadFavoritesMichis();
